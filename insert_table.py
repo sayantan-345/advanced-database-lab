321eb33a3cafed9c08bab8f03de18d81113dd0b2
sayantan-345 <64397618+sayantan-345@users.noreply.github.com>
2023-11-06 08:15:53 +0530

    Add files via upload

--- insert_table.py
+++ insert_table.py
@@ -1,13 +1,4 @@
-#import mysql.connector
-#conn=mysql.connector.connect(host='localhost',user='root',password='Marcosbose1234@')
 
-#mycursor=conn.cursor()
-'''for i in range(0,8):
-    print("enter data for row",i+1)
-    course_id=int(input("enter the number:-"))
-    coursename=(input("enter the name:-"))
-    credits=int(input("enter the number:-"))
-    mycursor.execute("insert into sayantan.course(course_id,coursename,credits),'values(%d,%s,%d)';")'''
 '''mycursor.execute('insert sayantan.student values(2,"Jane Smith","jane.smith@example.com","987-654-3210","456 Elm St");')
 mycursor.execute('insert sayantan.student values(3,"Robert Johnson","robert.j@example.com","555-123-4567","789 Oak Ave");')
 mycursor.execute('insert sayantan.student values(4,"Emily White","emily.white@example.com","111-222-3333","567 Pine St");')
@@ -18,19 +9,19 @@ mycursor.execute('insert sayantan.student values(8,"Melissa Turner","melissa.tur
 '''values = [('course_id{}'.format(i), 'coursename{}'.format(i),'credits{}'.format(i)) for i in range(0,8)]
 c = conn.cursor()
 c.executemany("INSERT INTO sayantan.course(course_id, coursename,credits) VALUES(?,?)", values)'''
-import mysql.connector
-
-def insert_varibles_into_table(course_id, coursename, credits):
+'''import mysql.connector
+from datetime import datetime
+def insert_varibles_into_table(examid, examdate, examtime,examlocation):
     try:
         connection = mysql.connector.connect(host='localhost',user='root',password='Marcosbose1234@')
         cursor = connection.cursor()
-        mySql_insert_query = """INSERT INTO Laptop (course_id, coursename, credits) 
-                                VALUES (%s, %s, %s, %s) """
+        mySql_insert_query = """INSERT INTO sayantan.exam (examid, examdate,examtime,examlocation) 
+                                VALUES (%s, %s, %s,%s) """
 
-        record = (course_id, coursename, credits)
+        record = (examid, examdate, examtime,examlocation)
         cursor.execute(mySql_insert_query, record)
         connection.commit()
-        print("Record inserted successfully into Laptop table")
+        print("Record inserted successfully into course table")
 
     except mysql.connector.Error as error:
         print("Failed to insert into MySQL table {}".format(error))
@@ -41,12 +32,167 @@ def insert_varibles_into_table(course_id, coursename, credits):
             connection.close()
             print("MySQL connection is closed")
 
-
-#insert_varibles_into_table(101, 'Mathematics', 6999, '2019-04-14')
-#insert_varibles_into_table(3, 'MacBook Pro', 2499, '2019-06-20')
-#conn.commit()
-#conn.close()
 for i in range(0,8):
-    course_id=int(input("enter the number"))
-    coursename=(input("enter coursename"))
-    credits=int(input("enter the credit"))
+    examid=int(input("enter the number"))
+    examdate=(input("enter exam date"))
+    examtime=(input("enter the examtime"))
+    #parsetime=datetime.strptime(examtime, "%I:%M %p")
+    #formattedtime = parsetime.strftime("%H:%M:%S")
+    examlocation=(input("enter the exam location"))
+    insert_varibles_into_table(examid, examdate, examtime, examlocation)'''
+'''import mysql.connector
+from datetime import datetime
+def insert_varibles_into_table( facultyid,name,email,phone,department):
+    try:
+        connection = mysql.connector.connect(host='localhost',user='root',password='Marcosbose1234@')
+        cursor = connection.cursor()
+        mySql_insert_query = """INSERT INTO sayantan.faculty ( facultyid,name,email,phone,department) 
+                                VALUES (%s, %s, %s,%s,%s) """
+
+        record = ( facultyid,name,email,phone,department)
+        cursor.execute(mySql_insert_query, record)
+        connection.commit()
+        print("Record inserted successfully into faculty table")
+
+    except mysql.connector.Error as error:
+        print("Failed to insert into MySQL table {}".format(error))
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+for i in range(0,8):
+    facultyid=int(input("enter the number"))
+    name=(input("enter name"))
+    email=(input("enter the email"))
+    #parsetime=datetime.strptime(examtime, "%I:%M %p")
+    #formattedtime = parsetime.strftime("%H:%M:%S")
+    phone=(input("enter the phone number"))
+    department=(input("enter the department"))
+    insert_varibles_into_table(facultyid,name,email,phone,department)'''
+'''import mysql.connector
+from datetime import datetime
+def insert_varibles_into_table( enrollmentid,studentid,courseid,enrollmentdate):
+    try:
+        connection = mysql.connector.connect(host='localhost',user='root',password='Marcosbose1234@')
+        cursor = connection.cursor()
+        mySql_insert_query = """INSERT INTO sayantan.enrollment ( enrollmentid,student_id,course_id,enrollmentdate) 
+                                VALUES (%s, %s, %s,%s) """
+
+        record = (enrollmentid,studentid,courseid,enrollmentdate)
+        cursor.execute(mySql_insert_query, record)
+        connection.commit()
+        print("Record inserted successfully into faculty table")
+
+    except mysql.connector.Error as error:
+        print("Failed to insert into MySQL table {}".format(error))
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+for i in range(0,10):
+    enrollmentid=int(input("enter the number"))
+    studentid=int(input("enter name"))
+    courseid=int(input("enter the email"))
+    #parsetime=datetime.strptime(examtime, "%I:%M %p")
+    #formattedtime = parsetime.strftime("%H:%M:%S")
+    enrollmentdate=(input("enter the phone number"))
+    insert_varibles_into_table(enrollmentid,studentid,courseid,enrollmentdate)'''
+'''import mysql.connector
+from datetime import datetime
+def insert_varibles_into_table(teachingid,facultyid,course_id):
+    try:
+        connection = mysql.connector.connect(host='localhost',user='root',password='Marcosbose1234@')
+        cursor = connection.cursor()
+        mySql_insert_query = """INSERT INTO sayantan.teaching ( teachingid,facultyid,course_id) 
+                                VALUES (%s, %s, %s) """
+
+        record = (teachingid,facultyid,course_id)
+        cursor.execute(mySql_insert_query, record)
+        connection.commit()
+        print("Record inserted successfully into faculty table")
+
+    except mysql.connector.Error as error:
+        print("Failed to insert into MySQL table {}".format(error))
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+for i in range(0,10):
+    teachingid=int(input("enter the number"))
+    facultyid=int(input("enter name"))
+    course_id=int(input("enter the email"))
+    #parsetime=datetime.strptime(examtime, "%I:%M %p")
+    #formattedtime = parsetime.strftime("%H:%M:%S")
+    #enrollmentdate=(input("enter the phone number"))
+    insert_varibles_into_table(teachingid,facultyid,course_id)'''
+'''import mysql.connector
+from datetime import datetime
+def insert_varibles_into_table( registrationid,student_id,examid,registrationdate):
+    try:
+        connection = mysql.connector.connect(host='localhost',user='root',password='Marcosbose1234@')
+        cursor = connection.cursor()
+        mySql_insert_query = """INSERT INTO sayantan.examregistration ( registrationid,student_id,examid,registrationdate) 
+                                VALUES (%s, %s, %s,%s) """
+
+        record = (registrationid,student_id,examid,registrationdate)
+        cursor.execute(mySql_insert_query, record)
+        connection.commit()
+        print("Record inserted successfully into faculty table")
+
+    except mysql.connector.Error as error:
+        print("Failed to insert into MySQL table {}".format(error))
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+for i in range(0,10):
+    registrationid=int(input("enter the number"))
+    student_id=int(input("enter name"))
+    examid=int(input("enter the email"))
+    #parsetime=datetime.strptime(examtime, "%I:%M %p")
+    #formattedtime = parsetime.strftime("%H:%M:%S")
+    registrationdate=(input("enter the phone number"))
+    insert_varibles_into_table(registrationid,student_id,examid,registrationdate)'''
+'''import mysql.connector
+from datetime import datetime
+def insert_varibles_into_table( resultid,student_id,examid,score):
+    try:
+        connection = mysql.connector.connect(host='localhost',user='root',password='Marcosbose1234@')
+        cursor = connection.cursor()
+        mySql_insert_query = """INSERT INTO sayantan.examresult ( resultid,student_id,examid,score) 
+                                VALUES (%s, %s, %s,%s) """
+
+        record = (resultid,student_id,examid,score)
+        cursor.execute(mySql_insert_query, record)
+        connection.commit()
+        print("Record inserted successfully into faculty table")
+
+    except mysql.connector.Error as error:
+        print("Failed to insert into MySQL table {}".format(error))
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+for i in range(0,10):
+    resultid=int(input("enter the number"))
+    student_id=int(input("enter name"))
+    examid=int(input("enter the email"))
+    #parsetime=datetime.strptime(examtime, "%I:%M %p")
+    #formattedtime = parsetime.strftime("%H:%M:%S")
+    score=(input("enter the phone number"))
+    insert_varibles_into_table(resultid,student_id,examid,score)'''
